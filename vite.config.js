414651f98f35bf8b03f03eba5d4882cb7f6e10b7
mahmoudgshake <mahmoud_gamal1430@yahoo.com>
2024-09-11 03:27:54 +0300

    Updates

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: '/num2verify-deploy/',  // Replace 'repo-name' with your GitHub repository name
-  plugins: [react()],
-})
